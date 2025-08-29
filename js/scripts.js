189f76a5bc2464fceb147857795d4fb88643c1e0
Oleksandr Yekimov (Sashka_UA_1) <sasaekimov@gmail.com>
2025-08-29 19:41:35 +0300

    NewStart29082025

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,1 @@
-import './setTheme.js';
-import './setActiveItemsMenu.js';
-import './toggleMenu.js';
-import './sliderSkills.js';
-import './sliderProject.js';
-import './toggleLanguages.js';
+// new code
